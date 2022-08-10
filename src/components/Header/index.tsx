2332384b578c33b594c647bdd397636b7c56144a
matheusyanmonteiro <matheusyanmonteiro@gmail.com>
2022-08-10 00:06:59 -0300

    bomb commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import * as C from './styles';
+export const Header = () => {
+  return (
+    <C.Container>
+      <h1>Cadastro de Desenvolvedor</h1>
+      <p>Faça seu cadastro na lista e receba um bolo ao final.</p>
+    </C.Container>
+  );
+}
